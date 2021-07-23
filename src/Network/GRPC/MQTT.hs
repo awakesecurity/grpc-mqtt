e14894a291d96e6720a2f81bd93b0087c71a3d95
Matt Friede <friede80@gmail.com>
2021-07-23 17:02:48 -0400

    Added Apache license and source file headers

--- src/Network/GRPC/MQTT.hs
+++ src/Network/GRPC/MQTT.hs
@@ -1,3 +1,9 @@
+{- 
+  Copyright (c) 2021 Arista Networks, Inc.
+  Use of this source code is governed by the Apache License 2.0
+  that can be found in the COPYING file.
+-}
+
 module Network.GRPC.MQTT (
   module Network.MQTT.Client
 )where
--- test/Test.hs
+++ test/Test.hs
@@ -1,3 +1,9 @@
+{- 
+  Copyright (c) 2021 Arista Networks, Inc.
+  Use of this source code is governed by the Apache License 2.0
+  that can be found in the COPYING file.
+-}
+
 {-# LANGUAGE OverloadedLists #-}
 
 module Main where

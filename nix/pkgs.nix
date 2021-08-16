6d70c64e81c0a77ae5579c272d1bcde3f8eade75
Jake <jacob.leach@arista.com>
2021-08-16 16:29:53 -0500

    change: refactor nix files

---
+++ nix/pkgs.nix
@@ -0,0 +1,9 @@
+{ compiler }:
+
+import ./nixpkgs.nix {
+  overlays = [
+    (import ./overlays/haskell-packages.nix {
+      inherit compiler;
+    })
+  ];
+}
--- shell.nix
+++ shell.nix
@@ -1,1 +1,19 @@
-(import ./release.nix).shell
+{ compiler ? "ghc8104"
+}:
+
+let
+  pkgs = import ./nix/pkgs.nix {
+    inherit compiler;
+  };
+
+  grpc-mqtt = pkgs.haskellPackages.grpc-mqtt;
+
+in grpc-mqtt.env.overrideAttrs (old: {
+  buildInputs = (old.buildAttrs or []) ++ [
+    pkgs.cabal-install
+    pkgs.grpc
+    pkgs.haskell-language-server
+    pkgs.haskellPackages.c2hs
+    pkgs.haskellPackages.proto3-suite
+  ];
+})

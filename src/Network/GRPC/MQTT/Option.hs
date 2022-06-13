d621683f2bd274ae11fa20446b3695e7be89b5de
Jake Leach <jacobleach@protonmail.com>
2022-06-13 12:04:03 -0500

    add: `ProtoOptions` type

--- src/Network/GRPC/MQTT/Option.hs
+++ src/Network/GRPC/MQTT/Option.hs
@@ -1,19 +1,26 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskellQuotes #-}
+
 -- | TODO
 --
 -- @since 0.1.0.0
 module Network.GRPC.MQTT.Option
-  ( -- * TODO
+  ( -- * ProtoOptions
+    ProtoOptions
+      ( ProtoOptions,
+        rpcBatchStream,
+        rpcServerCLevel,
+        rpcClientCLevel
+      ),
 
-    -- * File Options
-
-    -- * Option Types
-
-
-    -- ** Batched
+    -- * Batched
     -- $option-batched
     Batched (Batch, Batched, Unbatched, getBatched),
 
-    -- ** CLevel
+    -- * CLevel
     -- $option-clevel
     CLevel (CLevel, getCLevel),
     fromCLevel,
@@ -22,6 +29,16 @@ module Network.GRPC.MQTT.Option
 where
 
 ---------------------------------------------------------------------------------
+
+import Data.Data (Data)
+
+import Proto3.Suite
+  ( Message (decodeMessage, dotProto, encodeMessage),
+    Named,
+    Primitive (primType),
+  )
+
+import Relude
 
 ---------------------------------------------------------------------------------
 
@@ -34,31 +51,29 @@ import Network.GRPC.MQTT.Option.CLevel
     toCLevel,
   )
 
----------------------------------------------------------------------------------
-
--- File Options -----------------------------------------------------------------
+-- ProtoOptions -----------------------------------------------------------------
 
 -- | TODO
 --
--- @since 0.1.0.0
-
--- Service Options --------------------------------------------------------------
-
--- | TODO
+-- @
+-- // import the compression level enumeration "CLevel"
+-- import "haskell/grpc/mqtt/clevel.proto";
+--
+-- message ProtoOptions {
+--   bool rpc_batch_stream = 1;
+--   haskell.grpc.mqtt.CLevel rpc_server_clevel = 2;
+--   haskell.grpc.mqtt.CLevel rpc_client_clevel = 3;
+-- }
+-- @
 --
 -- @since 0.1.0.0
-
--- Method Options ---------------------------------------------------------------
-
--- | TODO
---
--- @since 0.1.0.0
-
--- Option Types -----------------------------------------------------------------
-
--- | TODO
---
--- @since 0.1.0.0
+data ProtoOptions = ProtoOptions
+  { rpcBatchStream :: Batched
+  , rpcServerCLevel :: Maybe CLevel
+  , rpcClientCLevel :: Maybe CLevel
+  }
+  deriving stock (Eq, Data, Generic, Ord, Show, Typeable)
+  deriving anyclass (Named)
 
 -- Batched ----------------------------------------------------------------------
 

40774e67522382c80744ff554a10c61f0bc08735
Jake Leach <jacobleach@protonmail.com>
2022-06-28 03:18:55 -0500

    changes: `Network.GRPC.MQTT.Proto` changes/additions
    Additions:
    - `DatumRep` and `DotProtoValue` reification utility functions
    - `ProtoIOError` exception type to represent failure modes performing I/O on `*.proto` files
    -  `throwIO` functions raising `ProtoIOError` exception in `IO`
    - `openProtoFileIO` for readining the contents of `*.proto` files with proper error handling
    - `ProtoOptionError` exception type to represent malformed profobuf options usage
    - Protobuf options type checking
    - Complete haddock TODOs
    - `proto3-suite` orphan instances.
    
    Changes:
    - Prevent setting duplicate option values in `ProtoOptionSet`
    - Rename `castDatumRep` to `castDatumRepWith`

--- src/Network/GRPC/MQTT/TH/Proto.hs
+++ src/Network/GRPC/MQTT/TH/Proto.hs
@@ -1,31 +1,66 @@
 -- Copyright (c) 2021 Arista Networks, Inc.
 -- Use of this source code is governed by the Apache License 2.0
 -- that can be found in the COPYING file.
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 
--- |
-module Network.GRPC.MQTT.TH.Proto where
+-- | TODO
+--
+-- @since 0.1.0.0
+module Network.GRPC.MQTT.TH.Proto
+  ( -- * Template Haskell
+    openProtoFileQ,
+    makeProtoOptionMapQ,
+    queryMethodOptionsQ,
 
-import Relude
+    -- * ProtoOptionMap
+    ProtoOptionMap,
+    emptyProtoOptionMap,
+    lookupProtoOptionMap,
+    insertProtoOptionMap,
 
-import Network.GRPC.MQTT.Wrapping
-  ( wrapBiDiStreamingClientHandler,
-    wrapClientStreamingClientHandler,
-    wrapServerStreamingClientHandler,
-    wrapUnaryClientHandler,
+    -- * OptionReader
+    OptionReader,
+    makeProtoOptionConfig,
+
+    -- ** Context
+    OptionReaderCtx (OptionReaderCtx, ctxOptionDefs, ctxSetOptions),
+
+    -- ** Reading
+    readFileProtoOptions,
+    readServiceProtoOptions,
+    readMethodProtoOptions,
+
+    -- ** Query
+    queryOptionConfig,
+    queryOptionConfigM,
+
+    -- * TODO
+    forEachService,
   )
+where
 
-import Language.Haskell.TH (ExpQ, Name, Q, mkName)
-import Network.GRPC.MQTT.Types (Batched (Batched, Unbatched))
+--------------------------------------------------------------------------------
+
+import Control.Monad.Except (Except, MonadError, runExcept, throwError)
+
+import Data.Map.Strict qualified as Map
+
+import Language.Haskell.TH (ExpQ, Name, Q)
+import Language.Haskell.TH qualified as TH
+import Language.Haskell.TH.Syntax qualified as TH
+
+import Proto3.Suite.DotProto qualified as DotProto
 import Proto3.Suite.DotProto.AST
-  ( DotProto (protoDefinitions, protoPackage),
-    DotProtoDefinition (DotProtoService),
-    DotProtoIdentifier (Single),
+  ( DotProto,
+    DotProtoIdentifier,
     DotProtoOption (dotProtoOptionIdentifier),
+    DotProtoPackageSpec,
     DotProtoServicePart (DotProtoServiceOption, DotProtoServiceRPCMethod),
     DotProtoValue (BoolLit),
+    Path,
     RPCMethod
       ( RPCMethod,
         rpcMethodName,
@@ -40,39 +75,292 @@ import Proto3.Suite.DotProto.AST
   )
 import Proto3.Suite.DotProto.Internal
   ( CompileError,
-    dpIdentQualName,
-    dpIdentUnqualName,
-    importProto,
     invalidMethodNameError,
     prefixedFieldName,
-    protoPackageName,
-    typeLikeName,
   )
-import Turtle (FilePath, directory, filename)
+import Proto3.Suite.DotProto.Internal qualified as DotProto
 
--------------------------------------------------------------------------------
+import Relude
+
+import Turtle qualified
+
+--------------------------------------------------------------------------------
+
+import Network.GRPC.MQTT.Option
+  ( ProtoOptions
+      ( ProtoOptions,
+        rpcBatchStream,
+        rpcClientCLevel,
+        rpcServerCLevel
+      ),
+    defaultProtoOptions,
+  )
+import Network.GRPC.MQTT.Proto
+  ( ProtoDatum,
+    ProtoOptionError (OptionValueTypeMismatch),
+    ProtoOptionSet,
+    ProtoService (ProtoService),
+    castDatum,
+    datumRepOf,
+    getFileOptions,
+    getFileServices,
+    getMethodOptions,
+    getServiceOptions,
+    lookupOption,
+    methodsOf,
+    openProtoFileIO,
+    servicesOf,
+    throwCompileErrorIO,
+  )
+import Network.GRPC.MQTT.Types (Batched (Batched, Unbatched))
+import Network.GRPC.MQTT.Wrapping
+  ( wrapBiDiStreamingClientHandler,
+    wrapClientStreamingClientHandler,
+    wrapServerStreamingClientHandler,
+    wrapUnaryClientHandler,
+  )
+
+--------------------------------------------------------------------------------
+
+-- | TODO
+--
+-- @since 0.1.0.0
+openProtoFileQ :: Turtle.FilePath -> Q DotProto
+openProtoFileQ filepath = do
+  dotproto <- liftIO (openProtoFileIO filepath)
+  TH.addDependentFile (Turtle.encodeString filepath)
+  pure dotproto
+
+-- | TODO
+--
+-- @since 0.1.0.0
+makeProtoOptionMapQ :: DotProto -> Q ProtoOptionMap
+makeProtoOptionMapQ dotproto =
+  case makeProtoOptionConfig dotproto of
+    Left err -> fail (displayException err)
+    Right os -> pure os
+
+-- | TODO
+--
+-- @since 0.1.0.0
+queryMethodOptionsQ :: RPCMethod -> ProtoOptionMap -> Q ProtoOptions
+queryMethodOptionsQ rpc optmap =
+  case lookupProtoOptionMap rpc optmap of
+    Nothing -> fail ("internal error: missing options the RPC " ++ show rpc)
+    Just os -> pure os
+
+--------------------------------------------------------------------------------
+
+-- | TODO
+--
+-- @since 0.1.0.0
+newtype ProtoOptionMap = ProtoOptionMap
+  {getProtoOptionConfig :: Map RPCMethod ProtoOptions}
+  deriving (Eq, Ord, Show)
+
+-- | TODO
+--
+-- @since 0.1.0.0
+emptyProtoOptionMap :: ProtoOptionMap
+emptyProtoOptionMap = ProtoOptionMap Map.empty
+
+-- | TODO
+--
+-- @since 0.1.0.0
+lookupProtoOptionMap :: RPCMethod -> ProtoOptionMap -> Maybe ProtoOptions
+lookupProtoOptionMap key (ProtoOptionMap kvs) = Map.lookup key kvs
+
+-- | TODO
+--
+-- @since 0.1.0.0
+insertProtoOptionMap :: RPCMethod -> ProtoOptions -> ProtoOptionMap -> ProtoOptionMap
+insertProtoOptionMap key val (ProtoOptionMap kvs) = ProtoOptionMap (Map.insert key val kvs)
+
+-- OptionReader ----------------------------------------------------------------
+
+-- | TODO
+--
+-- @since 0.1.0.0
+newtype OptionReader a = OptionReader
+  { unOptionReader ::
+      ReaderT OptionReaderCtx (StateT ProtoOptionMap (Except ProtoOptionError)) a
+  }
+  deriving newtype (Functor, Applicative, Monad)
+  deriving newtype
+    ( MonadError ProtoOptionError
+    , MonadReader OptionReaderCtx
+    , MonadState ProtoOptionMap
+    )
+
+-- | TODO
+--
+-- @since 0.1.0.0
+data OptionReaderCtx = OptionReaderCtx
+  { ctxOptionDefs :: ProtoOptions
+  , ctxSetOptions :: ProtoOptionSet
+  }
+
+-- | TODO
+--
+-- @since 0.1.0.0
+makeProtoOptionConfig :: DotProto -> Either ProtoOptionError ProtoOptionMap
+makeProtoOptionConfig dotproto = do
+  opts <- getFileOptions dotproto
+  let ctx :: OptionReaderCtx
+      ctx = OptionReaderCtx defaultProtoOptions opts
+   in unOptionReader makeFileConfig
+        & flip runReaderT ctx
+        & flip execStateT emptyProtoOptionMap
+        & runExcept
+  where
+    setLocalDefaults :: ProtoOptions -> OptionReader a -> OptionReader a
+    setLocalDefaults opts = local \ctx -> ctx{ctxOptionDefs = opts}
+
+    setLocalOptions :: ProtoOptionSet -> OptionReader a -> OptionReader a
+    setLocalOptions opts = local \ctx -> ctx{ctxSetOptions = opts}
+
+    -- Traverses the toplevel 'DotProto' @dotproto@ to populate the
+    -- 'OptionConfig' state.
+    makeFileConfig :: OptionReader ()
+    makeFileConfig = do
+      -- Within a 'DotProto' (the top level syntax for protobuf files), a new
+      -- 'ProtoOptions' is constructed from the file-level protobuf options .
+      --
+      -- Set @defs@ to use as the default 'ProtoOptions' value for all services
+      -- defined in @dotproto@.
+      defs <- readFileProtoOptions
+      setLocalDefaults defs do
+        () <$ servicesOf dotproto \service -> do
+          optset <- getServiceOptions service
+          setLocalOptions optset (makeServiceConfig service)
+
+    -- Traverses each service defined in the file @dotproto@ to populate the
+    -- 'OptionConfig' state.
+    makeServiceConfig :: ProtoService -> OptionReader ()
+    makeServiceConfig service = do
+      -- Set @defs@ to be the default 'ProtoOptions' value for all RPC methods
+      -- that are defined within the scope of @service@.
+      defs <- readServiceProtoOptions
+      setLocalDefaults defs do
+        () <$ methodsOf service \rpc -> do
+          optset <- getMethodOptions rpc
+          setLocalOptions optset (makeMethodConfig rpc)
+
+    -- Sets the 'ProtoOptions' configuration for a single RPC method.
+    makeMethodConfig :: RPCMethod -> OptionReader ()
+    makeMethodConfig rpc = do
+      -- Record @opts@ in the options configuration whenever the 'ProtoOptions'
+      -- set for @method@ differ from the default options set for @dotproto@.
+      opts <- readMethodProtoOptions
+      modify (insertProtoOptionMap rpc opts)
+
+-- OptionReader - Reading ------------------------------------------------------
+
+-- | TODO
+--
+-- @since 0.1.0.0
+readFileProtoOptions :: OptionReader ProtoOptions
+readFileProtoOptions = do
+  defs <- asks ctxOptionDefs
+  ProtoOptions
+    <$> queryOptionConfig "batched_stream_file" (rpcBatchStream defs)
+    <*> queryOptionConfigM "server_clevel_file" (rpcServerCLevel defs)
+    <*> queryOptionConfigM "client_clevel_file" (rpcClientCLevel defs)
+
+-- | TODO
+--
+-- @since 0.1.0.0
+readServiceProtoOptions :: OptionReader ProtoOptions
+readServiceProtoOptions = do
+  defs <- asks ctxOptionDefs
+  ProtoOptions
+    <$> queryOptionConfig "batched_stream_service" (rpcBatchStream defs)
+    <*> queryOptionConfigM "server_clevel_service" (rpcServerCLevel defs)
+    <*> queryOptionConfigM "client_clevel_service" (rpcClientCLevel defs)
+
+-- | TODO
+--
+-- @since 0.1.0.0
+readMethodProtoOptions :: OptionReader ProtoOptions
+readMethodProtoOptions = do
+  defs <- asks ctxOptionDefs
+  ProtoOptions
+    <$> queryOptionConfig "batched_stream" (rpcBatchStream defs)
+    <*> queryOptionConfigM "server_clevel" (rpcServerCLevel defs)
+    <*> queryOptionConfigM "client_clevel" (rpcClientCLevel defs)
+
+-- OptionReader - Query --------------------------------------------------------
+
+-- | TODO
+--
+-- @since 0.1.0.0
+queryOptionConfig :: ProtoDatum a => String -> a -> OptionReader a
+queryOptionConfig nm def = do
+  opts <- asks ctxSetOptions
+  let idt :: DotProtoIdentifier
+      idt = makeQualifiedId nm
+   in case lookupOption idt opts of
+        Nothing -> pure def
+        Just val -> castProtoOption (DotProto.DotProtoOption idt val)
+
+-- | TODO
+--
+-- @since 0.1.0.0
+queryOptionConfigM ::
+  ProtoDatum a =>
+  String ->
+  Maybe a ->
+  OptionReader (Maybe a)
+queryOptionConfigM nm def = do
+  opts <- asks ctxSetOptions
+  let idt :: DotProtoIdentifier
+      idt = makeQualifiedId nm
+   in case lookupOption idt opts of
+        Nothing -> pure def
+        Just val -> do
+          val' <- castProtoOption (DotProto.DotProtoOption idt val)
+          pure (Just val')
+
+-- | TODO
+--
+-- @since 0.1.0.0
+castProtoOption :: forall a. ProtoDatum a => DotProtoOption -> OptionReader a
+castProtoOption opt =
+  let val :: DotProtoValue
+      val = DotProto.dotProtoOptionValue opt
+   in case castDatum val of
+        Just px -> pure px
+        Nothing ->
+          let issue :: ProtoOptionError
+              issue = OptionValueTypeMismatch (datumRepOf val) opt
+           in throwError issue
+
+makeQualifiedId :: String -> DotProtoIdentifier
+makeQualifiedId nm =
+  let path :: Path
+      path = DotProto.Path ("haskell" :| ["grpc", "mqtt", nm])
+   in DotProto.Dots path
+
+--------------------------------------------------------------------------------
 
 -- | Proto option to enable/disable batching in streams
 batchedStreamOptionIdent :: DotProtoIdentifier
-batchedStreamOptionIdent = Single "hs_grpc_mqtt_batched_stream"
+batchedStreamOptionIdent = DotProto.Single "hs_grpc_mqtt_batched_stream"
 
 forEachService ::
   Turtle.FilePath ->
   Batched ->
   (String -> [(String, Batched, ExpQ, Name)] -> ExceptT CompileError Q a) ->
   Q [a]
-forEachService protoFilepath defBatchedStream action = showErrors . runExceptT $ do
-  let protoFile = filename protoFilepath
-      protoDir = directory protoFilepath
+forEachService filepath defBatchedStream action = do
+  dotproto <- openProtoFileQ filepath
 
-  dotproto <- importProto [protoDir] protoFile protoFile
-  packageName <- dpIdentQualName =<< protoPackageName (protoPackage dotproto)
+  pkgName <- toPackageNameQ (DotProto.protoPackage dotproto)
+  pkgQual <- toQualifiedNameQ pkgName
 
-  let services = [(name, parts) | DotProtoService _ name parts <- protoDefinitions dotproto]
-
-  forM services $ \(name, parts) -> do
-    serviceName <- typeLikeName =<< dpIdentUnqualName name
-    let endpointPrefix = "/" <> packageName <> "." <> serviceName <> "/"
+  forM (getFileServices dotproto) \(ProtoService nm ps) -> do
+    serviceName <- toUnqualifiedNameQ nm
+    let endpointPrefix = "/" ++ pkgQual ++ "." ++ serviceName ++ "/"
 
     let isBatchedStreamEnabled :: [DotProtoOption] -> Maybe Batched
         isBatchedStreamEnabled opts =
@@ -84,7 +372,7 @@ forEachService protoFilepath defBatchedStream action = showErrors . runExceptT $
            in isBatched <$> maybeOpt
 
     let serviceUsesBatchedStream :: Maybe Batched
-        serviceUsesBatchedStream = isBatchedStreamEnabled $ mapMaybe toOption parts
+        serviceUsesBatchedStream = isBatchedStreamEnabled $ mapMaybe toOption ps
           where
             toOption (DotProtoServiceOption opt) = Just opt
             toOption _ = Nothing
@@ -94,7 +382,7 @@ forEachService protoFilepath defBatchedStream action = showErrors . runExceptT $
 
     let serviceMethodName (DotProtoServiceRPCMethod method@RPCMethod{..}) = do
           case rpcMethodName of
-            Single nm -> do
+            DotProto.Single rpcNm -> do
               let useBatchedStream = fromMaybe defBatchedStream $ methodUsesBatchedStream method <|> serviceUsesBatchedStream
 
               let streamingWrapper =
@@ -103,12 +391,26 @@ forEachService protoFilepath defBatchedStream action = showErrors . runExceptT $
                       (NonStreaming, NonStreaming) -> [e|wrapUnaryClientHandler|]
                       (Streaming, NonStreaming) -> [e|wrapClientStreamingClientHandler|]
                       (Streaming, Streaming) -> [e|wrapBiDiStreamingClientHandler useBatchedStream|]
-              clientFun <- prefixedFieldName serviceName nm
-              return [(endpointPrefix <> nm, useBatchedStream, streamingWrapper, mkName clientFun)]
-            _ -> invalidMethodNameError rpcMethodName
+              clientFun <- mapCompileErrorQ (prefixedFieldName serviceName rpcNm)
+              return [(endpointPrefix <> rpcNm, useBatchedStream, streamingWrapper, TH.mkName clientFun)]
+            _ -> mapCompileErrorQ (invalidMethodNameError rpcMethodName)
         serviceMethodName _ = pure []
 
-    serviceMethods <- foldMapM serviceMethodName parts
-    action serviceName serviceMethods
+    serviceMethods <- foldMapM serviceMethodName ps
+    result <- runExceptT (action serviceName serviceMethods)
+    mapCompileErrorQ result
   where
-    showErrors = fmap (either (fail . show) id)
+    toPackageNameQ :: DotProtoPackageSpec -> Q DotProtoIdentifier
+    toPackageNameQ pkg = mapCompileErrorQ (DotProto.protoPackageName pkg)
+
+    toUnqualifiedNameQ :: DotProtoIdentifier -> Q String
+    toUnqualifiedNameQ idt = mapCompileErrorQ do
+      nm <- DotProto.dpIdentUnqualName idt
+      DotProto.typeLikeName nm
+
+    toQualifiedNameQ :: DotProtoIdentifier -> Q String
+    toQualifiedNameQ idt = mapCompileErrorQ (DotProto.dpIdentQualName idt)
+
+    mapCompileErrorQ :: Either CompileError a -> Q a
+    mapCompileErrorQ (Left err) = liftIO (throwCompileErrorIO filepath err)
+    mapCompileErrorQ (Right x) = pure x
--- src/Proto3/Suite/Orphans.hs
+++ src/Proto3/Suite/Orphans.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
+
 -- | TODO
 --
 -- @since 0.1.0.0
@@ -7,15 +8,24 @@ module Proto3.Suite.Orphans () where
 ---------------------------------------------------------------------------------
 
 import Data.Data (Data)
+import Data.Ord (Ord)
 
 import Proto3.Suite.DotProto
-  ( DotProtoValue (..),
-    DotProtoIdentifier (..),
+  ( DotProtoIdentifier (..),
+    DotProtoOption (..),
+    DotProtoValue (..),
     Path (..),
+    RPCMethod (..),
+    Streaming (..),
   )
 
 ---------------------------------------------------------------------------------
 
-deriving instance Data DotProtoValue
 deriving instance Data DotProtoIdentifier
+deriving instance Data DotProtoOption
+deriving instance Data DotProtoValue
 deriving instance Data Path
+
+-- Needed so that 'RPCMethod' can be used to key 'Data.Map.Strict.Map'
+deriving instance Ord RPCMethod
+deriving instance Ord Streaming

42117050fcdcb1212a67165cb70dd87f3163e141
Raghu Kaippully <raghuk@arista.com>
2022-09-29 10:34:02 +0530

    Adjust docs

--- nix/overlays/haskell.nix
+++ nix/overlays/haskell.nix
@@ -21,6 +21,8 @@ final: prev: {
 
             grpc-mqtt = (hfinal.callCabal2nix "grpc-mqtt" (gitignore.lib.gitignoreSource ../..) { }).overrideAttrs (old: {
               buildInputs = (old.buildInputs or []) ++ [ final.mosquitto ];
+
+              # The tests require a running mosquitto server
               preCheck = "bash ./scripts/host-mosquitto.sh -d &";
               postCheck = "xargs --arg-file=test-files/mqtt-broker.pid kill";
             });

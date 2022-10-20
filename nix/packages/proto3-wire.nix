b422ec3dc35b97239787874522b62e7c9c8a163d
Jake Leach <jacob.leach@arista.com>
2022-10-19 23:39:29 -0500

    change: update pinned proto3-wire revision

--- nix/packages/proto3-wire.nix
+++ nix/packages/proto3-wire.nix
@@ -9,8 +9,8 @@ mkDerivation {
   version = "1.4.0";
   src = fetchgit {
     url = "https://github.com/awakesecurity/proto3-wire";
-    sha256 = "1pr078k7j5yvsixh7g76bfb3w5a5nxsjl9lgvc5ji4nm0ngx5i61";
-    rev = "ae24c00c83cbce29750005b1fa6506c1e62e4822";
+    sha256 = "059na8az87cl6h0h9q5q6jnkbnw38rgywa1x1rrvihnh99bj7g2v";
+    rev = "baf23455bda0c5a316711f7d91aa86f43c934e7f";
     fetchSubmodules = true;
   };
   libraryHaskellDepends = [

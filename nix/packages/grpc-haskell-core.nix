68874c2a05600345d18aaf8a419022d60aa6590a
jake <jacob.leach@arista.com>
2023-07-27 19:12:39 -0500

    Update `grpc-haskell` and `grpc-haskell-core` pins to revision `d20c20d` (#66)
    * update: `grpc-haskell(-core)` to rev `d20c20d`

--- nix/packages/grpc-haskell-core.nix
+++ nix/packages/grpc-haskell-core.nix
@@ -7,9 +7,9 @@ mkDerivation {
   pname = "grpc-haskell-core";
   version = "0.5.0";
   src = fetchgit {
-    url = "https://github.com/awakesecurity/gRPC-haskell";
-    sha256 = "0sh553lvz3vi1mq65jicy6n3ga4zcifabvapi217kpivxjsa18g6";
-    rev = "414ae8e6612e8e28d2bcfb6e201303f5fc031e5a";
+    url = "https://github.com/awakesecurity/gRPC-haskell.git";
+    sha256 = "17wjm9lbyzhm98g4g36v3jlnr00s5yzilrv90p12mqgn3ffhkg28";
+    rev = "d20c20d63c170b5eaf5725f03f7b7060352af402";
     fetchSubmodules = true;
   };
   postUnpack = "sourceRoot+=/core; echo source root reset to $sourceRoot";

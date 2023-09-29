60289d79a0fcd0e79ef4d49960b3b0e8d9d74c1e
Matt Friede <friede80@gmail.com>
2023-09-29 16:23:51 -0400

    Switch OrderedTQueue implementation to use pqueue

--- src/Control/Concurrent/OrderedTQueue.hs
+++ src/Control/Concurrent/OrderedTQueue.hs
@@ -9,69 +9,58 @@
 -- Definitions for the 'OrderedTQueue' container.
 --
 -- @since 1.0.0
-module Control.Concurrent.OrderedTQueue where
+module Control.Concurrent.OrderedTQueue3 where
 
 --------------------------------------------------------------------------------
 
 import Relude
 
-import Control.Concurrent.STM.TQueue
-  ( TQueue,
-    isEmptyTQueue,
-    newTQueue,
-    newTQueueIO,
-    readTQueue,
-    writeTQueue,
-  )
-import Data.SortedList qualified as SL
+import Control.Concurrent.STM (retry)
+import Data.PQueue.Min
 
 data Indexed a = Indexed
   { idx :: !Int32
   , val :: !a
   }
+  deriving stock (Show)
 instance Eq (Indexed a) where
   (==) = (==) `on` idx
 
 instance Ord (Indexed a) where
   (<=) = (<=) `on` idx
 
-data OrderedTQueue a = OrderedTQueue (TVar Int32) (TVar (SL.SortedList (Indexed a))) (TQueue (Indexed a))
+data OrderedTQueue a = OrderedTQueue (TVar Int32) (TVar (MinQueue (Indexed a)))
 
 newOrderedTQueue :: STM (OrderedTQueue a)
 newOrderedTQueue =
   OrderedTQueue
     <$> newTVar 0
     <*> newTVar mempty
-    <*> newTQueue
 
 newOrderedTQueueIO :: IO (OrderedTQueue a)
 newOrderedTQueueIO = do
   OrderedTQueue
     <$> newTVarIO 0
     <*> newTVarIO mempty
-    <*> newTQueueIO
 
 writeOrderedTQueue :: OrderedTQueue a -> Indexed a -> STM ()
-writeOrderedTQueue (OrderedTQueue _ _ queue) = writeTQueue queue
-
-isEmptyOrderedTQueue :: OrderedTQueue a -> STM Bool
-isEmptyOrderedTQueue (OrderedTQueue _ _ queue) = isEmptyTQueue queue
+writeOrderedTQueue (OrderedTQueue _ queueVar) x = modifyTVar' queueVar (insert x)
 
 readOrderedTQueue :: OrderedTQueue a -> STM a
-readOrderedTQueue otq@(OrderedTQueue seqVar bufferVar queue) = do
+readOrderedTQueue otq@(OrderedTQueue seqVar queueVar) = do
   curSeq <- readTVar seqVar
-  buffer <- readTVar bufferVar
+  queue <- readTVar queueVar
 
-  ia@Indexed{idx, val} <-
-    case SL.uncons buffer of
-      Just (ia, rest)
-        | curSeq == idx ia -> writeTVar bufferVar rest $> ia
-      _ -> readTQueue queue
-
-  if idx < 0
-    then return val
-    else case idx `compare` curSeq of
-      -- If sequence number is less than current, it must be a duplicate, so we discard it and read again
-      LT -> readOrderedTQueue otq
-      EQ -> modifyTVar' seqVar (+ 1) $> val
-      GT -> modifyTVar' bufferVar (SL.insert ia) >> readOrderedTQueue otq
+  case minView queue of
+    Just (ia, rest) ->
+      case idx ia `compare` curSeq of
+        LT -> do
+          -- If sequence number is less than current, it must be a duplicate, so we discard it and read again
+          writeTVar queueVar rest
+          readOrderedTQueue otq
+        EQ -> do
+          writeTVar queueVar rest
+          modifyTVar' seqVar (+ 1)
+          pure (val ia)
+        GT -> retry
+    Nothing -> retry

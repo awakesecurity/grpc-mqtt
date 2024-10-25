b19ab0b5b1f82ef096d92e73e12da962c4655670
Tomas Drtina <tdrtina-ext@arista.com>
2024-10-25 17:09:08 +0200

    proto3-suite: update to 0.8.1

--- nix/packages/proto3-suite.nix
+++ nix/packages/proto3-suite.nix
@@ -1,7 +1,7 @@
 { mkDerivation, aeson, aeson-pretty, attoparsec, base
 , base64-bytestring, binary, bytestring, cereal, containers
 , contravariant, deepseq, dhall, doctest, fetchgit, filepath, foldl
-, generic-arbitrary, hashable, haskell-src, hedgehog
+, generic-arbitrary, ghc, hashable, hedgehog
 , insert-ordered-containers, large-generics, large-records, lens
 , lib, mtl, neat-interpolation, optparse-applicative
 , optparse-generic, parsec, parsers, pretty, pretty-show
@@ -12,11 +12,11 @@
 }:
 mkDerivation {
   pname = "proto3-suite";
-  version = "0.7.0";
+  version = "0.8.1";
   src = fetchgit {
     url = "https://github.com/awakesecurity/proto3-suite.git";
-    sha256 = "16x6acbcd4sqscjqdwmp0j6rlfn7lczp3vsyqv6v8r8zxwm244dk";
-    rev = "30599c127cc53838576992a33c8c9d91fd57b429";
+    sha256 = "1msy068045zbcw1wr99qf2cqjcd5065bn2qcipsc9hdqz6v35s65";
+    rev = "800bfa8ddd6e9a32eedca1005560648ba97084cf";
     fetchSubmodules = true;
   };
   isLibrary = true;
@@ -25,19 +25,19 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson aeson-pretty attoparsec base base64-bytestring binary
     bytestring cereal containers contravariant deepseq dhall filepath
-    foldl hashable haskell-src insert-ordered-containers large-generics
+    foldl ghc hashable insert-ordered-containers large-generics
     large-records lens mtl neat-interpolation parsec parsers pretty
     pretty-show proto3-wire QuickCheck quickcheck-instances safe split
     swagger2 system-filepath text text-short time transformers turtle
     vector
   ];
   executableHaskellDepends = [
-    base containers mtl optparse-applicative optparse-generic
+    base containers ghc mtl optparse-applicative optparse-generic
     proto3-wire range-set-list system-filepath text turtle
   ];
   testHaskellDepends = [
     aeson attoparsec base base64-bytestring bytestring cereal
-    containers deepseq doctest generic-arbitrary hedgehog
+    containers deepseq doctest generic-arbitrary ghc hedgehog
     large-generics large-records mtl parsec pretty pretty-show
     proto3-wire QuickCheck record-hasfield swagger2 tasty
     tasty-hedgehog tasty-hunit tasty-quickcheck text text-short

3ad69172cf29446ad1e5b0c71bf402c793d4fc42
John Carey <jcarey@arista.com>
2025-05-18 13:21:06 -0700

    Support proto3-suite v0.9.2

--- nix/packages/proto3-suite.nix
+++ nix/packages/proto3-suite.nix
@@ -1,47 +1,45 @@
-{ mkDerivation, aeson, aeson-pretty, attoparsec, base
-, base64-bytestring, binary, bytestring, cereal, containers
-, contravariant, deepseq, dhall, doctest, fetchgit, filepath, foldl
-, generic-arbitrary, ghc, hashable, hedgehog
-, insert-ordered-containers, large-generics, large-records, lens
-, lib, mtl, neat-interpolation, optparse-applicative
-, optparse-generic, parsec, parsers, pretty, pretty-show
-, proto3-wire, QuickCheck, quickcheck-instances, range-set-list
+{ mkDerivation, aeson, aeson-pretty, attoparsec, attoparsec-aeson
+, base, base64-bytestring, binary, bytestring, cereal, containers
+, contravariant, deepseq, doctest, fetchgit, filepath, foldl
+, generic-arbitrary, ghc-lib-parser, hashable, hedgehog
+, insert-ordered-containers, lens, lib, mtl, neat-interpolation
+, optparse-applicative, optparse-generic, parsec, parsers, pretty
+, pretty-show, proto3-wire, QuickCheck, quickcheck-instances
 , record-hasfield, safe, split, swagger2, system-filepath, tasty
-, tasty-hedgehog, tasty-hunit, tasty-quickcheck, text, text-short
-, time, transformers, turtle, vector
+, tasty-hedgehog, tasty-hunit, tasty-quickcheck, template-haskell
+, text, text-short, time, transformers, turtle, vector
 }:
 mkDerivation {
   pname = "proto3-suite";
-  version = "0.8.1";
+  version = "0.9.2";
   src = fetchgit {
     url = "https://github.com/awakesecurity/proto3-suite.git";
-    sha256 = "1msy068045zbcw1wr99qf2cqjcd5065bn2qcipsc9hdqz6v35s65";
-    rev = "800bfa8ddd6e9a32eedca1005560648ba97084cf";
+    sha256 = "1nw7r4l1148v961bdxswb93ziqdg07lf6yfv555xr5vsx7fa8i92";
+    rev = "dac138b2d1d179ba79824aad8acd126ad61ae935";
     fetchSubmodules = true;
   };
   isLibrary = true;
   isExecutable = true;
   enableSeparateDataOutput = true;
   libraryHaskellDepends = [
-    aeson aeson-pretty attoparsec base base64-bytestring binary
-    bytestring cereal containers contravariant deepseq dhall filepath
-    foldl ghc hashable insert-ordered-containers large-generics
-    large-records lens mtl neat-interpolation parsec parsers pretty
-    pretty-show proto3-wire QuickCheck quickcheck-instances safe split
-    swagger2 system-filepath text text-short time transformers turtle
-    vector
+    aeson aeson-pretty attoparsec attoparsec-aeson base
+    base64-bytestring binary bytestring cereal containers contravariant
+    deepseq filepath foldl ghc-lib-parser hashable
+    insert-ordered-containers lens mtl neat-interpolation parsec
+    parsers pretty pretty-show proto3-wire QuickCheck
+    quickcheck-instances safe split swagger2 system-filepath
+    template-haskell text text-short time transformers turtle vector
   ];
   executableHaskellDepends = [
-    base containers ghc mtl optparse-applicative optparse-generic
-    proto3-wire range-set-list system-filepath text turtle
+    base containers ghc-lib-parser mtl optparse-applicative
+    optparse-generic proto3-wire system-filepath text turtle
   ];
   testHaskellDepends = [
     aeson attoparsec base base64-bytestring bytestring cereal
-    containers deepseq doctest generic-arbitrary ghc hedgehog
-    large-generics large-records mtl parsec pretty pretty-show
-    proto3-wire QuickCheck record-hasfield swagger2 tasty
-    tasty-hedgehog tasty-hunit tasty-quickcheck text text-short
-    transformers turtle vector
+    containers deepseq doctest generic-arbitrary ghc-lib-parser
+    hedgehog mtl parsec pretty pretty-show proto3-wire QuickCheck
+    record-hasfield swagger2 tasty tasty-hedgehog tasty-hunit
+    tasty-quickcheck text text-short transformers turtle vector
   ];
   description = "A higher-level API to the proto3-wire library";
   license = lib.licenses.asl20;

29aecea441298fe9bc78b78a947c2df98caae731
j6carey <j6carey@gmail.com>
2025-05-19 12:08:37 -0700

    Support proto3-suite v0.9.2 (#78)

--- nix/packages/proto3-wire.nix
+++ nix/packages/proto3-wire.nix
@@ -1,21 +1,26 @@
 { mkDerivation, base, bytestring, cereal, containers, criterion
-, deepseq, doctest, hashable, lib, parameterized, primitive
-, QuickCheck, random, safe, tasty, tasty-hunit, tasty-quickcheck
-, template-haskell, text, text-short, transformers
-, unordered-containers, vector, word-compat
+, deepseq, doctest, fetchgit, hashable, lib, parameterized
+, primitive, QuickCheck, random, safe, tasty, tasty-hunit
+, tasty-quickcheck, template-haskell, text, text-short
+, transformers, unordered-containers, vector, word-compat
 }:
 mkDerivation {
   pname = "proto3-wire";
-  version = "1.4.3";
-  sha256 = "7fa316e7d309bd298ce6f28b85b5bb3d7ec9ce5ada895e2531febedb3f064b3f";
+  version = "1.4.5";
+  src = fetchgit {
+    url = "https://github.com/awakesecurity/proto3-wire.git";
+    sha256 = "0nb8xy723jhybrfsyfcgpczgbv80hcndprc45h3zq9hliam07qqv";
+    rev = "d4376fb6f1c1ac03ee8ec5c5793700ca6508ea70";
+    fetchSubmodules = true;
+  };
   libraryHaskellDepends = [
     base bytestring cereal containers deepseq hashable parameterized
     primitive QuickCheck safe template-haskell text text-short
     transformers unordered-containers vector word-compat
   ];
   testHaskellDepends = [
-    base bytestring cereal doctest QuickCheck tasty tasty-hunit
-    tasty-quickcheck text text-short transformers vector
+    base bytestring cereal containers doctest QuickCheck tasty
+    tasty-hunit tasty-quickcheck text text-short transformers vector
   ];
   benchmarkHaskellDepends = [ base bytestring criterion random ];
   description = "A low-level implementation of the Protocol Buffers (version 3) wire format";
